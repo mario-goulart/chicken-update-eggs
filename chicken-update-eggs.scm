ae15b67a678a792261375b5a7d915928930e1839
Mario Domenech Goulart <mario.goulart@gmail.com>
2012-10-01 11:02:30 -0300

    Add --skip-eggs command line option

--- chicken-update-eggs.scm
+++ chicken-update-eggs.scm
@@ -1,40 +1,35 @@
-(use posix utils srfi-1 http-client)
+(use posix utils srfi-1 irregex)
 
-(define henrietta-uri
-  (make-parameter "http://code.call-cc.org/cgi-bin/henrietta.cgi"))
-
-
-(define (list-remote-eggs)
-  (with-input-from-request
-   (string-append (henrietta-uri) "?list=1")
-   #f
-   read-file))
+(define *deploy-dir* (get-environment-variable "CUE_DEPLOY_DIR"))
 
 
 (define (chicken-tool prefix tool)
-  (let ((tool-file (make-pathname (list prefix "bin")
+  (let ((tool-file (make-pathname (if *deploy-dir*
+                                      #f ;; Get chicken tools from path
+                                      (list prefix "bin"))
                                   tool
                                   (and (eq? (software-type) 'windows) "exe"))))
-    (unless (file-exists? tool-file)
+    (when (and (not *deploy-dir*) (not (file-exists? tool-file)))
       (print "Could not find " tool-file ". Aborting"))
     tool-file))
 
 
-(define (list-local-eggs prefix)
-  (map (compose string->symbol pathname-file)
-       (glob (make-pathname (list prefix
-                                  "lib"
-                                  "chicken"
-                                  (number->string (##sys#fudge 42)))
-                            "*" "setup-info"))))
-
-
-(define egg?
-  (let ((remote-eggs #f))
-    (lambda (module)
-      (unless remote-eggs
-        (set! remote-eggs (list-remote-eggs)))
-      (and (memq module remote-eggs) #t))))
+(define (list-eggs prefix)
+  (let ((setup-infos
+         (glob (make-pathname (list prefix
+                                    "lib"
+                                    "chicken"
+                                    (number->string (##sys#fudge 42)))
+                              "*" "setup-info")))
+        (eggs '()))
+    (for-each (lambda (setup-info)
+                (and-let* ((setup-info-data (with-input-from-file setup-info read))
+                           (egg (alist-ref 'egg-name setup-info-data))
+                           (egg (string->symbol (car egg))))
+                  (unless (memq egg eggs)
+                    (set! eggs (cons egg eggs)))))
+              setup-infos)
+    eggs))
 
 
 (define (install-eggs! to-prefix eggs #!key dry-run?)
@@ -46,24 +41,39 @@
       ((if dry-run?
            print
            system*)
-       (string-append chicken-install " "
+       (string-append chicken-install
+                      (if *deploy-dir*
+                          (string-append " -deploy -p " *deploy-dir*)
+                          "")
+                      " "
                       (string-intersperse
                        (map symbol->string eggs)))))))
 
 
-(define (eggs-to-install installed-stuff)
-  (filter egg? installed-stuff))
+
+(define (cmd-line-arg option args)
+  ;; Returns the argument associated to the command line option OPTION
+  ;; in ARGS or #f if OPTION is not found in ARGS or doesn't have any
+  ;; argument.
+  (let ((val (any (lambda (arg)
+                    (irregex-match
+                     `(seq ,(->string option) "=" (submatch (* any)))
+                     arg))
+                  args)))
+    (and val (irregex-match-substring val 1))))
 
 
 (define (usage #!optional exit-code)
   (print "Usage: " (pathname-strip-directory (program-name))
-         " [ --dry-run ] <from prefix> <to prefix>\n\n"
+         " [ <options> ] <from prefix> <to prefix>\n\n"
          "<from prefix> is the chicken installation prefix where "
          "to get the egg list to install into <to prefix>.\n\n"
+         "<options> are:\n"
+         "  --dry-run           only shows what is to be executed without actually executing it\n"
+         "  --skip-eggs=<eggs>  don't attempt to install <eggs> (a comma-sparated list og eggs)\n\n"
          "Example:\n\n"
          "    $ chicken-update-eggs /usr/local/chicken-4.7.0 /usr/local/chicken-4.7.4\n\n"
-         "would install all eggs from /usr/local/chicken-4.7.0 into /usr/local/chicken-4.7.4\n\n"
-         "--dry-run (optional) only shows what is to be executed without actually executing it.\n\n"
+         "would install all eggs from /usr/local/chicken-4.7.0 into /usr/local/chicken-4.7.4\n"
          "This program can also be used to update all eggs if you give it the same values for "
          "<from prefix> and <to prefix>.")
   (when exit-code (exit exit-code)))
@@ -78,9 +88,15 @@
             (member "-help" args)
             (member "--help" args))
     (usage 0))
-  (let ((from-prefix (last (butlast args)))
-        (to-prefix (last args))
-        (dry-run? (and (member "--dry-run" args) #t)))
+  (let* ((from-prefix (last (butlast args)))
+         (to-prefix (last args))
+         (skip (and-let* ((skip (cmd-line-arg '--skip-eggs args)))
+                 (map string->symbol (string-split skip ","))))
+         (dry-run? (and (member "--dry-run" args) #t)))
     (install-eggs! to-prefix
-                   (eggs-to-install (list-local-eggs from-prefix))
+                   (if skip
+                       (remove (lambda (egg)
+                                 (memq egg skip))
+                               (list-eggs from-prefix))
+                       (list-eggs from-prefix))
                    dry-run?: dry-run?)))
